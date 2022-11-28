ea4b003431fa827840259460f37f6e24f0e91726
Михаил Брагин <mbragins@mail.ru>
2022-11-28 17:50:10 +0500

    Second commit

--- yatube/core/models.py
+++ yatube/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class CreatedModel(models.Model):
     """Абстрактная модель. Добавляет дату создания."""
     pub_date = models.DateTimeField(
@@ -8,5 +9,5 @@ class CreatedModel(models.Model):
         db_index=True
     )
 
-    class Meta:      
-        abstract = True 
+    class Meta:
+        abstract = True
